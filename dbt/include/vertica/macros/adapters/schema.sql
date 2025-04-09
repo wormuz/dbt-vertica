fe4e8aad32ac284867451cd28be2db71fb92813a
Konstantin Cheremisov <konstantin.cheremisov@together.com>
2025-04-09 17:39:21 +0300

    Add schema creation toggle to Vertica adapter
    Introduced a configurable flag to enable or disable schema creation. This allows users to skip schema creation through the `create_schema` config or variable. Ensures better flexibility in managing schema setup workflows.

--- dbt/include/vertica/macros/adapters/schema.sql
+++ dbt/include/vertica/macros/adapters/schema.sql
@@ -1,4 +1,8 @@
 {% macro vertica__create_schema(relation) -%}
+  {%- set create_schema = config.get('create_schema', true) -%}
+  {%- if not create_schema or not var('create_schema', true) -%}
+    {{ return('') }}
+  {%- endif -%}
   {%- call statement('create_schema') -%}
     create schema if not exists {{ relation.without_identifier().include(database=False) }}
   {% endcall %}
